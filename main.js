1699c863ff59b72250012967b8a1039a1e996f11
Pasindu sadanjana <131373684+pasinduwickramasuriya@users.noreply.github.com>
2024-02-15 01:51:31 +0530

    wedsite
    create a website

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings: ["frontend Developer" , "freelancer" , "Graphic Designer" , "web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+var yped = new Typed(".wecome", {
+    strings: ["Welcome" , "to" , "the" , "site"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
